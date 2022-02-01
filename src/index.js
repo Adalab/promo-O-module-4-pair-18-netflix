811a313ce0d8414fec161fd08ceeef462c4bb737
Rocío Dols Pividal <rdolspividal@gmail.com>
2022-02-02 00:31:43 +0100

    Ejercicios Netflix Express JS 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-
+//Importamos los datos de las peliculas
+//La constante movies es un array con la info de las peliculas
+const movies = require('./data/movies.json');
 // create and config server
 const server = express();
 server.use(cors());
@@ -10,4 +12,33 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', (req, res) => {
+  //Consoleamos los query params que recibimos del fetch
+  console.log(req.query);
+  //Guardamos el valor del parametro gender en una constante
+  const genderFilterParam = req.query.gender;
+  const sortParam = req.query.sort;
+
+  //Filtramos por nombre de pelicula
+  //Hacemos un ternario para tener en cuenta que no queremos aplicar el filtro
+  //Si el valor del filtro es '' devolvemos siempre true, de forma que se agregaran todas las peliculas al array de peliculas filtradas. En caso contrario, compararemos el genero de cada una de las peliculas del array con el parametro que nos envian
+  const filteredMovies = movies.filter(movie => genderFilterParam === '' ? true : movie.gender === genderFilterParam);
+
+  //Ordenamos las peliculas por el order que nos envian como parametro
+  filteredMovies.sort((a, b) => {
+    if (sortParam === 'asc' && a.title > b.title || sortParam === 'desc' && a.title < b.title) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  //Esto es lo que va a recibir el fetch como response
+  //Modificamos el array de movies por la constante que importamos del archivo data/movies.json
+  res.json({
+    success: true,
+    movies: filteredMovies
+  });
 });
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,31 +1,39 @@
 // login
 
-const getMoviesFromApi = () => {
+const getMoviesFromApi = (params) => {
+  console.log(params);
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
+  //Hemos modificado la ruta para que apunte al endpoint /movies del servidor
+  return fetch(`http://localhost:4000/movies?gender=${params.gender}&sort=${params.sort}`)
     .then(response => response.json())
-    .then(() => {
+    .then((data) => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: true,
-        movies: [
-          {
-            id: '1',
-            title: 'Gambita de dama',
-            gender: 'Drama',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
-          },
-          {
-            id: '2',
-            title: 'Friends',
-            gender: 'Comedia',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
-          }
-        ]
-      };
+      //Borramos el codigo que habia para pintar los datos que devuelve el servidor
+      // return {
+      //   success: true,
+      //   movies: [
+      //     {
+      //       id: '1',
+      //       title: 'Gambita de dama',
+      //       gender: 'Drama',
+      //       image:
+      //         '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
+      //     },
+      //     {
+      //       id: '2',
+      //       title: 'Friends',
+      //       gender: 'Comedia',
+      //       image:
+      //         '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
+      //     }
+      //   ]
+      // };
+
+      //PIntamos los datos que nos devuelve el servidor
+      return data;
+
+
     });
 };
 
